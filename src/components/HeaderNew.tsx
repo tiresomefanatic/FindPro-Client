40864560295c19d31f1ee193cde5cda1a01d3ea5
Suprateek <suprateek@Suprateeks-MacBook-Pro.local>
2024-06-27 02:20:00 +0530

    gooooogle

--- src/components/HeaderNew.tsx
+++ src/components/HeaderNew.tsx
@@ -113,16 +113,15 @@ const Header = () => {
         <div className="flex flex-row justify-center items-center pr-20 max-md:hidden">
           {!isAuthenticated ? (
             <div>
-            
+              <Link href={`${baseURL}/auth/google-login`}>
                 <Button
                   variant="outline"
                   size="sm"
                   className="ml-4"
-                  onClick={handleGoogleLogin}
                 >
                   Login with Google
                 </Button>
-         
+              </Link>
             </div>
           ) : (
             <div className="flex flex-row gap-x-7 justify-center items-center">
@@ -154,15 +153,15 @@ const Header = () => {
               <div className="flex flex-col space-y-4">
                 {!isAuthenticated ? (
                   <div>
+                    <Link href={`${baseURL}auth/google-login`}>
                       <Button
                         variant="outline"
                         size="sm"
                         className="ml-4"
-                        onClick={handleGoogleLogin}
                       >
                         Login with Google
                       </Button>
-                
+                    </Link>
                   </div>
                 ) : (
                   <div className="flex flex-col gap-y-2">
