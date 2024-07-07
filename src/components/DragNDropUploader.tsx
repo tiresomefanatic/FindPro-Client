31785773d32aa4d8800fa1cfa10a3e8751ca0e89
Suprateek <suprateek@Suprateeks-MBP.Dlink>
2024-07-08 01:51:08 +0530

    prgoress bar

--- src/components/DragNDropUploader.tsx
+++ src/components/DragNDropUploader.tsx
@@ -262,8 +262,8 @@ export default function DragNDropUploader({ onUploadStatusChange }: DragNDropUpl
       }
         </div>
       ) : (
-        <div className="">
-          <p className="text-md text-gray-400">Uploading Files...</p>
+        <div className="bg-red-100">
+          <p className="text-md text-gray-400 bg-green-300">Uploading Files...</p>
           <Progress value={uploadProgress} className="w-full max-w-lg" />
         </div>
       )}
