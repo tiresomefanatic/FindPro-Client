10c9fe343e6b89fae806669cf8c9c05d0085cd0e
Suprateek <suprateek@Suprateeks-MBP.Dlink>
2024-07-10 20:54:38 +0530

    tooltip changed to popover component for touch screens

--- src/components/LabelWithTooltip.tsx
+++ src/components/LabelWithTooltip.tsx
@@ -6,7 +6,13 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import Image from 'next/image';
+import { useMediaQuery } from "@uidotdev/usehooks";
 
 interface LabelWithTooltipProps {
   label?: string;
@@ -15,32 +21,49 @@ interface LabelWithTooltipProps {
 }
 
 const LabelWithTooltip: React.FC<LabelWithTooltipProps> = ({ label, tooltipText, tooltipImage }) => {
+  const isExtraLargeDevice = useMediaQuery("(min-width: 1201px)");
+
   const handleTooltipClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-   // e.stopPropagation();
   };
+
+  const ContentComponent = () => (
+    <div className="flex flex-col items-center">
+      <p className='text-semibold'>{tooltipText}</p>
+      {tooltipImage && (
+        <Image src={tooltipImage} height={500} width={500} alt="Tooltip" />
+      )}
+    </div>
+  );
 
   return (
     <div className="flex items-center space-x-2">
       <span>{label}</span>
-      <TooltipProvider>
-        <Tooltip delayDuration={200}>
-          <TooltipTrigger asChild>
-            <button onClick={handleTooltipClick} className="focus:outline-none">
+      {isExtraLargeDevice ? (
+        <TooltipProvider>
+          <Tooltip delayDuration={200}>
+            <TooltipTrigger asChild>
+              <button onClick={handleTooltipClick} className="focus:outline-none">
+                <Info className="h-4 w-4 text-muted-foreground" />
+              </button>
+            </TooltipTrigger>
+            <TooltipContent className="max-w-xs sm:max-w-md">
+              <ContentComponent />
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      ) : (
+        <Popover>
+          <PopoverTrigger asChild>
+            <button className="focus:outline-none">
               <Info className="h-4 w-4 text-muted-foreground" />
             </button>
-          </TooltipTrigger>
-          <TooltipContent className="max-w-xs sm:max-w-md">
-            <div className="flex flex-col items-center">
-            <p className='text-semibold'>{tooltipText}</p>
-
-              {tooltipImage && (
-                <Image src={tooltipImage} height={500} width={500} alt="Tooltip"  />
-              )}
-            </div>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+          </PopoverTrigger>
+          <PopoverContent className="w-80">
+            <ContentComponent />
+          </PopoverContent>
+        </Popover>
+      )}
     </div>
   );
 };
